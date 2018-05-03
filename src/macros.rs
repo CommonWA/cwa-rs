796202ba6c5efbd4fc1813d684a72abe401397f2
losfair <zhy20000919@hotmail.com>
2018-05-03 20:29:16 +0800

    macros: Fix println & eprintln

--- src/macros.rs
+++ src/macros.rs
@@ -1,17 +1,35 @@
 #[macro_export]
 macro_rules! println {
-    ($fmt:expr $(, $arg:tt)*) => ($crate::io::with_stdout(|h| {
-        use std::io::Write;
-        write!(h, concat!($fmt, "\n") $(, $arg)*).unwrap();
-    }))
+    () => (print!("\n"));
+    ($fmt:expr) => ({
+        $crate::io::with_stdout(|h| {
+            use std::io::Write;
+            write!(h, concat!($fmt, "\n")).unwrap();
+        });
+    });
+    ($fmt:expr, $($arg:tt)*) => ({
+        $crate::io::with_stdout(|h| {
+            use std::io::Write;
+            write!(h, concat!($fmt, "\n"), $($arg)*).unwrap();
+        });
+    });
 }
 
 #[macro_export]
 macro_rules! eprintln {
-    ($fmt:expr $(, $arg:tt)*) => ($crate::io::with_stderr(|h| {
-        use std::io::Write;
-        write!(h, concat!($fmt, "\n") $(, $arg)*).unwrap();
-    }))
+    () => (print!("\n"));
+    ($fmt:expr) => ({
+        $crate::io::with_stderr(|h| {
+            use std::io::Write;
+            write!(h, concat!($fmt, "\n")).unwrap();
+        });
+    });
+    ($fmt:expr, $($arg:tt)*) => ({
+        $crate::io::with_stderr(|h| {
+            use std::io::Write;
+            write!(h, concat!($fmt, "\n"), $($arg)*).unwrap();
+        });
+    });
 }
 
 #[macro_export]
